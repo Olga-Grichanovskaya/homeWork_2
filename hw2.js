3f429fbd61689f801b5ec0429df2eee6defa8578
Olga Grichanovskaya <olga.grichanovskaya@gmail.com>
2019-07-29 13:31:28 +0300

    part of tasks

--- hw2.js
+++ hw2.js
@@ -1,7 +1,7 @@
 //1
 const multiplyOrSumUp = (a, b) => a % 2 === 0 ? a * b : a + b;
 
-function getCoordinateQuarter(x, y) {
+const getCoordinateQuarter = (x, y) => {
     let quarter = "";
 
     if (x > 0 && y > 0) {
@@ -17,7 +17,7 @@ function getCoordinateQuarter(x, y) {
     return quarter;
 }
 
-function getSum(a, b, c) {
+const getSum = (a, b, c) => {
     let sum = 0;
 
     if (a > 0) {
@@ -33,23 +33,139 @@ function getSum(a, b, c) {
     return sum;
 }
 
+const sumUpMaxExpression = (a, b, c) => a * b * c > a + b + c ? a * b * c + 3 : a + b + c + 3;
 
-function getMarkByRating(rating) {
+
+const getMarkByRating = rating => {
     let mark = "";
             
-    if (rating > 0 && rating < 20) {
-        mark = "Оценка - F";
+    if (rating >= 0 && rating < 20) {
+        mark = "F";
     } else if (rating >= 20 && rating < 40) {
-        mark = "Оценка - E";
+        mark = "E";
     } else if (rating >= 40 && rating < 60) {
-        mark = "Оценка - D";
+        mark = "D";
     } else if (rating >= 60 && rating < 75) {
-        mark = "Оценка - C";
+        mark = "C";
     } else if (rating >= 75 && rating < 90) {
-        mark = "Оценка - B";
-    } else if (rating >= 90 && rating < 100) {
-        mark = "Оценка - A";
+        mark = "B";
+    } else if (rating >= 90 && rating <= 100) {
+        mark = "A";
+    } else {
+        throw new Error('Invalid argument provided!');
     }
 
     return mark;
 }
+
+const getSumOfEvenIndex = array => {
+	if(!array.length) {
+		return -1;
+	}
+
+	let sum = 0;
+
+	for(let i = 0; i < array.length; i++) {
+
+		 if(i % 2 === 0) {
+		 	sum += array[i];
+		}
+	}
+
+	return sum;
+}
+
+//3
+const getMinElemOfArr = array => {
+
+    if (!array.length) {
+        return -1;
+    }
+
+    let minElem = array[0];
+
+    for(let i = 1; i < array.length; i++) {
+
+        if (array[i] < minElem) {
+            minElem = array[i];
+        }
+
+    }
+
+    return minElem;
+}
+
+const getMaxElemOfArr = array => {
+
+    if(!array.length) {
+        return -1;
+    }
+
+    let maxElem = array[0];
+
+    for(let i = 1; i < array.length; i++) {
+
+        if(array[i] > maxElem) {
+            maxElem = array[i];
+        }
+
+    }
+
+    return maxElem;
+}
+
+const getMinIndex = array => {
+    if (!array.length) {
+        return -1;
+    }
+
+    let minIndex = 0;
+
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] < array[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
+
+const getMaxIndex = array => {
+	if(!array.length) {
+		return -1;
+	}
+	
+	let maxIndex = 0;
+
+	for(let i = 0; i < array.length; i++) {
+
+		if (array[i] > array[maxIndex]) {
+			maxIndex = i;
+		}
+	}
+
+	return maxIndex;
+}
+
+const reverseArr = array => {
+	if(!array.length) {
+		return -1;
+	}
+
+	for(let i = 0, j = array.length - 1; i < j; i++) {
+		array[j] = array[j] + array[i];
+		array[i] = array[j] - array[i];
+		j--;
+	}
+
+	return array;
+}
+
+
+
+
+
+
+// const getDay = (num) => {
+
+// }
--- hw2_test.js
+++ hw2_test.js
@@ -67,9 +67,6 @@ describe('Homework tasks', () => {
                 assert.strictEqual(actual, expected);
             });
 
-            it(`should return string ${expected}`, () => {
-                assert.isString(expected);
-            });
         });
     });
 
@@ -102,48 +99,45 @@ describe('Homework tasks', () => {
             const actual = getSum(a, b, c);
 
             //Then
-            it(`should return not undefined, result - ${expected}`, () => {
-                expect(expected).not.to.be.undefined;
-            });
             it(`should return sum ${expected} when a = ${a} and b = ${b} and c = ${c}`, () => {
                 assert.strictEqual(actual, expected);
             });
-            it(`should return a number - ${expected}`, () => {
-                assert.isNumber(expected);
-            });
+
         });
     });
 
-    describe('multiplyOrSumUp', () => {
+    describe('sumUpMaxExpression', () => {
         //Given
         const testData = [
             {
                 a: 2,
                 b: 3,
-                c: 9,
-                expected: 6,
+                c: 5,
+                expected: 33,
             },
             {
                 a: 1,
-                b: 4,
-                expected: 5,
+                b: 2,
+                c: 2,
+                expected: 8,
             },
             {
                 a: 0,
                 b: 1,
-                expected: 0,
+                c: 3,
+                expected: 7,
             }
         ];
         
         testData.forEach(data => {
-            // const {a, b, expected} = data;
+            const {a, b, c, expected} = data;
             // //When
-            // const actual = multiplyOrSumUp(a, b);
+            const actual = sumUpMaxExpression(a, b, c);
 
             // //Then
-            // it(`should return ${expected} when a = ${a} and b = ${b}`, () => {
-            //     assert.strictEqual(actual, expected);
-            // });
+            it(`should return ${expected} when a = ${a}  b = ${b} c = ${c}`, () => {
+                assert.strictEqual(actual, expected);
+            });
 
         });
     });
@@ -205,8 +199,52 @@ describe('Homework tasks', () => {
             {
                 rating: 61,
                 expected: 'C',
+            },
+            {
+                rating: 70,
+                expected: 'C',
+            },
+            {
+                rating: 74,
+                expected: 'C',
+            },
+            {
+                rating: 75,
+                expected: 'B',
+            },
+            {
+                rating: 76,
+                expected: 'B',
+            },
+            {
+                rating: 81,
+                expected: 'B',
+            },
+            {
+                rating: 89,
+                expected: 'B',
+            },
+            {
+                rating: 90,
+                expected: 'A',
+            },
+            {
+                rating: 91,
+                expected: 'A',
+            },
+            {
+                rating: 96,
+                expected: 'A',
+            },
+            {
+                rating: 100,
+                expected: 'A',
+            },
+            {
+                rating: 101,
+                expected: "Mark is not exist",
             }
-         ];
+        ];
  
         testData.forEach(data => {
             const { rating, expected } = data;
@@ -217,8 +255,150 @@ describe('Homework tasks', () => {
                 assert.strictEqual(actual, expected);
             });
 
+            // it(`should return ${expected}`, () => {
+            //     expect(function () {}).to.throw();
+            // })
+
 
         });
-     });
+    });
+
+    describe('getSumEvenIndex', () => {
+
+        it(`should return sum of even index`, () => {
+            const expected = 0;
+            getSumEvenIndex();
+            assert.strictEqual(actual, expected);
+        })
+    })
+
+    describe('getMinElemOfArr', () => {
+		//given
+		const testData = [
+			{
+				array: [],
+				expected: -1,
+			},
+			{
+				array: [3],
+				expected: 3,
+			},
+			{
+				array: [12, 9],
+				expected: 9,
+			},
+			{
+				array: [4, 8, 1, 5],
+				expected: 1,
+			}
+		];
+
+		testData.forEach(data => {
+			const {array, expected} = data;
+
+			const actual = getMinElemOfArr(array);
+
+			it(`should return min el ${expected} when array - [${array}]`, () => {
+		
+				assert.strictEqual(actual, expected);
+
+			});
+		});
+	});
+
+	describe('getMaxElemOfArr', () => {
+
+		const testData = [
+			{
+				array: [],
+				expected: -1,
+			},
+			{
+				array: [2],
+				expected: 2,
+			},
+			{
+				array: [7, 14],
+				expected: 14,
+			},
+			{
+				array: [3, 9, 20, 12],
+				expected: 20,
+			}
+		];
+
+		testData.forEach(data => {
+			const { array, expected } = data;
+
+			const actual = getMaxElemOfArr(array);
+ 
+			it(`should return max ${expected} when array - [${array}]`, () => {
+				assert.strictEqual(actual, expected);
+			});
+		});
+
+	});
+
+    describe('getMinIndex', () => {
+        const testData = [
+            {
+                array: [],
+                expected: -1,
+            },
+            {
+                array: [5],
+                expected: 0,
+            },
+            {
+                array: [3, 2],
+                expected: 1,
+            },
+            {
+                array: [2, 5, 6, 0, 7],
+                expected: 3,
+            }
+        ];
+
+        testData.forEach(data => {
+            const { array, expected } = data;
+
+            const actual = getMinIndex(array);
+
+            it(`should return ${expected} when array = [${array}]`, () => {
+                assert.strictEqual(actual, expected);
+            });
+        })
+    });
+
+    describe('getMaxIndex', () => {
+        const testData = [
+            {
+                arr: [],
+                expected: -1,
+            },
+            {
+
+            }
+        ];
+        
+        testData.forEach(data => {
+            const {arr, expected} = data;
+
+            const actual = getMaxIndex(arr);
+
+
+            it(`should return ${expected} max Index when array = [${arr}]`, () => {
+                asssert.strictEqual(actual, expected);
+            });
+
+
+        });
+
+
+
+    });
+
+
+
 
 });
